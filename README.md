2cf4eb188bc770a7c7d5aad8f701d8f990d6ca7f
Vince Momot <vincemomot@gmail.com>
2021-10-26 18:56:57 -0400

    Makes the readme with table of contents

--- README.md
+++ README.md
@@ -1,3 +1,61 @@
-# readme maker 
- ### Project Description 
- makes readmes
+
+# please wotk
+ 
+### Project Description
+this is ssoppused to have workign links
+
+
+### Table of contents
+1. [Installation Instructions](#i)
+2. [Project Usage](#u)
+3. [Project Guidelines](#g)
+4. [Test Instructions](#t)
+5. [Contact and GitHub Info](#c)
+
+
+
+### Installation Instructions
+<a name="i"></a>
+install npm instaill inquirer
+
+<a name="u"></a>
+### Usage Information
+usr theis ot make quick reamdes
+
+<a name="g"></a>
+### Guidelines
+sont deeat tmy stucc
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+### Test Instructions
+dont lose my wokt
+
+<a name="c"></a>
+###Contact and GitHub Info
+
+Github Username: 123456 
+ 
+email: vince@12456.com
--- index.js
+++ index.js
@@ -34,11 +34,16 @@ inquirer.prompt([
         message: "What are the test instructions?",
         name: "instructions"
     },
-    // {
-    //     type: "rawlist",
-    //     message: "What license do you need?",
-    //     name: "license"
-    // },
+    {
+        type: "rawlist",
+        message: "What license do you need?",
+        name: "license",
+        choices: [
+        'MIT',
+        'ISC',
+        'GPL'
+      ]
+    },
     {
         type: "input",
         message: "What is your github username?",
@@ -53,11 +58,40 @@ inquirer.prompt([
 
 ])
     .then(answers => {
-        fs.writeFile('README.md', `# ${answers.title} \n ### Project Description \n ${answers.description}`, (err) => err ? console.error(err) : console.log("New ReadMe Created"))
+        fs.writeFile('README.md',
+       `
+# ${answers.title}\n 
+## Project Description
+${answers.description}\n
+
+## Table of contents
+1. [Installation Instructions](#i)
+2. [Project Usage](#u)
+3. [Project Guidelines](#g)
+4. [Test Instructions](#t)
+5. [Contact and GitHub Info](#c)
+
+
+
+## Installation Instructions
+<a name="i"></a>
+${answers.install}\n
+<a name="u"></a>
+## Usage Information
+${answers.usage}\n
+<a name="g"></a>
+## Guidelines
+${answers.guidelines}\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n
+## Test Instructions
+${answers.instructions}\n
+<a name="c"></a>
+## Contact and GitHub Info \n
+Github Username: ${answers.github} \n 
+email: ${answers.email}`
+ ,
+
+            (err) => err ? console.error(err) : console.log("New ReadMe Created"))
     })
-
-// TODO: Create a function to write README file
-
 
 // TODO: Create a function to initialize app
 function init() { }
